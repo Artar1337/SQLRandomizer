d9b8a4fa867ba6c538832f38dafd769b7cdeb676
Artem <armastra@gmail.com>
2021-02-23 17:40:40 +0600

    Created float randomizer. Project formatted

--- app/src/main/java/ru/sibsutis/sqlrandomizer/MainActivity.java
+++ app/src/main/java/ru/sibsutis/sqlrandomizer/MainActivity.java
@@ -15,98 +15,101 @@ public class MainActivity extends AppCompatActivity {
         return random.nextInt(max - min) + min;
     }
 
-    private String generateName(boolean isMale)
-    {
-        String name="";
+    public static float getRandomFloat(int min, int max) {
+        Random random = new Random();
+        return (float) random.nextInt(max - min)
+                + min + random.nextFloat();
+    }
 
-        if(isMale)
-        {
-            String [] names = {"Артём"
-                    ,"Вадим"
-                    ,"Владислав"
-                    ,"Михаил"
-                    ,"Матвей"
-                    ,"Герман"
-                    ,"Андрей"
-                    ,"Артур"
-                    ,"Николай"
-                    ,"Илья"
-                    ,"Руслан"
-                    ,"Данил"
-                    ,"Даниил"};
-            String [] surnames = {"Астраханцев"
-                    ,"Гребенщиков"
-                    ,"Захаров"
-                    ,"Козин"
-                    ,"Переломов"
-                    ,"Сафонов"
-                    ,"Шаповалов"
-                    ,"Захаров"
-                    ,"Сарветников"
-                    ,"Андропов"
-                    ,"Берианидзе"
-                    ,"Потапов"
-                    ,"Потанин"
-                    ,"Андреев"
-                    ,"Кузьмин"
-                    ,"Мокроусов"
-                    ,"Браун"
-                    ,"Бонд"
-                    ,"Дорофеев"
-                    ,"Галанцев"};
-            int index = getRandomNumber(0,surnames.length-1);
-            name+=surnames[index]+" ";
-            index = getRandomNumber(0,names.length-1);
-            name+=names[index];
-        }
-        else
-        {
-            String [] names = {"Дарья"
-                    ,"Наталья"
-                    ,"Валерия"
-                    ,"Кристина"
-                    ,"Елизавета"
-                    ,"Яна"
-                    ,"Анастасия"
-                    ,"Виктория"
-                    ,"Алёна"
-                    ,"Людмила"
-                    ,"Джульетта"
-                    ,"Гермиона"
-                    ,"Трисс"};
-            String [] surnames = {"Астраханцева"
-                    ,"Гребенщикова"
-                    ,"Захарова"
-                    ,"Козина"
-                    ,"Переломова"
-                    ,"Сафонова"
-                    ,"Шаповалова"
-                    ,"Захарова"
-                    ,"Сарветникова"
-                    ,"Андропова"
-                    ,"Берианидзе"
-                    ,"Потапова"
-                    ,"Потанина"
-                    ,"Андреева"
-                    ,"Кузьмина"
-                    ,"Мокроусова"
-                    ,"Браун"
-                    ,"Бонд"
-                    ,"Дорофеева"
-                    ,"Галанцева"};
-            int index = getRandomNumber(0,surnames.length-1);
-            name+=surnames[index]+" ";
-            index = getRandomNumber(0,names.length-1);
-            name+=names[index];
+    private String generateName(boolean isMale) {
+        String name = "";
+
+        if (isMale) {
+            String[] names = {"Артём"
+                    , "Вадим"
+                    , "Владислав"
+                    , "Михаил"
+                    , "Матвей"
+                    , "Герман"
+                    , "Андрей"
+                    , "Артур"
+                    , "Николай"
+                    , "Илья"
+                    , "Руслан"
+                    , "Данил"
+                    , "Даниил"};
+            String[] surnames = {"Астраханцев"
+                    , "Гребенщиков"
+                    , "Захаров"
+                    , "Козин"
+                    , "Переломов"
+                    , "Сафонов"
+                    , "Шаповалов"
+                    , "Захаров"
+                    , "Сарветников"
+                    , "Андропов"
+                    , "Берианидзе"
+                    , "Потапов"
+                    , "Потанин"
+                    , "Андреев"
+                    , "Кузьмин"
+                    , "Мокроусов"
+                    , "Браун"
+                    , "Бонд"
+                    , "Дорофеев"
+                    , "Галанцев"};
+            int index = getRandomNumber(0, surnames.length - 1);
+            name += surnames[index] + " ";
+            index = getRandomNumber(0, names.length - 1);
+            name += names[index];
+        } else {
+            String[] names = {"Дарья"
+                    , "Наталья"
+                    , "Валерия"
+                    , "Кристина"
+                    , "Елизавета"
+                    , "Яна"
+                    , "Анастасия"
+                    , "Виктория"
+                    , "Алёна"
+                    , "Людмила"
+                    , "Джульетта"
+                    , "Гермиона"
+                    , "Трисс"};
+            String[] surnames = {"Астраханцева"
+                    , "Гребенщикова"
+                    , "Захарова"
+                    , "Козина"
+                    , "Переломова"
+                    , "Сафонова"
+                    , "Шаповалова"
+                    , "Захарова"
+                    , "Сарветникова"
+                    , "Андропова"
+                    , "Берианидзе"
+                    , "Потапова"
+                    , "Потанина"
+                    , "Андреева"
+                    , "Кузьмина"
+                    , "Мокроусова"
+                    , "Браун"
+                    , "Бонд"
+                    , "Дорофеева"
+                    , "Галанцева"};
+            int index = getRandomNumber(0, surnames.length - 1);
+            name += surnames[index] + " ";
+            index = getRandomNumber(0, names.length - 1);
+            name += names[index];
         }
         return name;
     }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.wtf("name is ",generateName(false));
-        Log.wtf("name is ",generateName(true));
+        Log.wtf("name is ", generateName(false));
+        Log.wtf("name is ", generateName(true));
     }
 }
